96a14eedca81f028445f4290845aa9ab989e32ee
BogdanATA <batameev@gmail.com>
2026-05-13 21:42:19 -0400

    created helper methods to handle the user input. Prevents program from crashing from bad input.

---
+++ README.md
@@ -0,0 +1,63 @@
+# Car World
+
+## Description of the Project
+
+This application lets users keep track of an inventory of vehicles at a dealership and add or remove them. The intended users are employees at a dealership who want to maintain an 
+inventory list of vehicles they have on hand. They can also add vehicles if new ones come in or remove them if they get sold. This application aims to help dealership employees
+manage their on hand inventory.
+
+## User Stories
+
+- As a user, I want to be able to be able to find a vehicle by price range, make/model, year range, color, mileage range, and vehicle type, so I could find exactly what car I am looking for.
+- As a user, I want to be able to see all available vehicles, so I can see all my possible options.
+- As a user, I want to be able to add a vehicle to the list, so I can add new vehicles that come into the dealership.
+- As a user, I want to be able to remove a vehicle from the list, so I can remove vehicles that have been sold.
+
+## Setup
+
+Instructions on how to set up and run the project using IntelliJ IDEA.
+
+### Prerequisites
+
+- IntelliJ IDEA: Ensure you have IntelliJ IDEA installed, which you can download from [here](https://www.jetbrains.com/idea/download/).
+- Java SDK: Make sure Java SDK is installed and configured in IntelliJ.
+
+### Running the Application in IntelliJ
+
+Follow these steps to get your application running within IntelliJ IDEA:
+
+1. Open IntelliJ IDEA.
+2. Select "Open" and navigate to the directory where you cloned or downloaded the project.
+3. After the project opens, wait for IntelliJ to index the files and set up the project.
+4. Find the main class with the `public static void main(String[] args)` method.
+5. Right-click on the file and select 'Run 'YourMainClassName.main()'' to start the application.
+
+## Technologies Used
+
+- Java: JDK 17
+
+## Demo
+
+![Application Screenshot]()
+
+## Future Work
+
+- 
+
+## Resources
+
+List resources such as tutorials, articles, or documentation that helped you during the project.
+
+- [Java Visual Learning Hub](https://raymaroun.github.io/yearup-java-visuals/index.html)
+- [RayMaroun solution Repos](https://github.com/RayMaroun/yearup-spring-section-8-2026)
+- [w3schools](https://www.w3schools.com/java/)
+
+## Team Members
+
+- **Bogdan Atamyeyev** - Lead developer.
+
+## Thanks
+
+- Thank you to [Raymond Maroun] for continuous support and guidance.
+- A special thanks to all my colleagues for their helpful advice.
+ 
--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -17,8 +17,8 @@ public class UserInterface {
         init();
         Scanner scanner = new Scanner(System.in);
 
-        int command = -1;
-        while (command != 99) {
+        String command = "";
+        while (!command.equals("99")) {
             System.out.println("\n--- " + dealership.getName() + " ---");
             System.out.println("1 - Search by price");
             System.out.println("2 - Search by make/model");
@@ -32,80 +32,67 @@ public class UserInterface {
             System.out.println("99 - Quit");
             System.out.print("Enter command: ");
 
-            command = scanner.nextInt();
-            scanner.nextLine();
+                command = scanner.nextLine();
 
-            switch (command) {
-                case 1 -> processGetByPriceRequest(scanner);
-                case 2 -> processGetByMakeModelRequest(scanner);
-                case 3 -> processGetByYearRequest(scanner);
-                case 4 -> processGetByColorRequest(scanner);
-                case 5 -> processGetByMileageRequest(scanner);
-                case 6 -> processGetByVehicleTypeRequest(scanner);
-                case 7 -> processGetAllVehiclesRequest();
-                case 8 -> processAddVehicleRequest(scanner);
-                case 9 -> processRemoveVehicleRequest(scanner);
-                case 99 -> System.out.println("Goodbye!");
-                default -> System.out.println("Invalid command.");
-            }
+                switch (command) {
+                    case "1" -> processGetByPriceRequest(scanner);
+                    case "2" -> processGetByMakeModelRequest(scanner);
+                    case "3" -> processGetByYearRequest(scanner);
+                    case "4" -> processGetByColorRequest(scanner);
+                    case "5" -> processGetByMileageRequest(scanner);
+                    case "6" -> processGetByVehicleTypeRequest(scanner);
+                    case "7" -> processGetAllVehiclesRequest();
+                    case "8" -> processAddVehicleRequest(scanner);
+                    case "9" -> processRemoveVehicleRequest(scanner);
+                    case "99" -> System.out.println("Goodbye!");
+                    default -> System.out.println("Invalid command.");
+                }
+
         }
         scanner.close();
     }
 
     public void processGetByPriceRequest(Scanner scanner) {
-        System.out.print("Please Enter a Min Price: ");
-        double minPrice = scanner.nextDouble();
-        System.out.print("Please Enter a Max Price: ");
-        double maxPrice = scanner.nextDouble();
-        scanner.nextLine();
+        double minPrice = readDouble(scanner, "Please Enter a Min Price: ");
+        double maxPrice = readDouble(scanner, "Please Enter a Max Price: ");
 
         List<Vehicle> vehicles = dealership.getVehiclesByPrice(minPrice, maxPrice);
         displayVehicles(vehicles);
     }
 
     public void processGetByMakeModelRequest(Scanner scanner) {
-        System.out.print("Please Enter a Make: ");
-        String make = scanner.nextLine();
-        System.out.print("Please Enter a Model: ");
-        String model = scanner.nextLine();
+        String make = readString(scanner, "Please Enter a Make: ");
+        String model = readString(scanner, "Please Enter a Model: ");
 
         List<Vehicle> vehicles = dealership.getVehiclesByMakeModel(make, model);
         displayVehicles(vehicles);
     }
 
     public void processGetByYearRequest(Scanner scanner) {
-        System.out.print("Please Enter a Min Year: ");
-        int minYear = scanner.nextInt();
-        System.out.print("Please Enter a Max Year: ");
-        int maxYear = scanner.nextInt();
-        scanner.nextLine();
+        int minYear = readInt(scanner, "Please Enter a Min Year: ");
+        int maxYear = readInt(scanner, "Please Enter a Max Year: ");
 
         List<Vehicle> vehicles = dealership.getVehiclesByYear(minYear, maxYear);
         displayVehicles(vehicles);
     }
 
     public void processGetByColorRequest(Scanner scanner) {
-        System.out.print("Please Enter a Color: ");
-        String color = scanner.nextLine();
+        String color = readString(scanner, "Please Enter a Color: ");
 
         List<Vehicle> vehicles = dealership.getVehiclesByColor(color);
         displayVehicles(vehicles);
     }
 
     public void processGetByMileageRequest(Scanner scanner) {
-        System.out.print("Please Enter a Min Mileage: ");
-        int minMileage = scanner.nextInt();
-        System.out.print("Please Enter a Max Mileage: ");
-        int maxMileage = scanner.nextInt();
-        scanner.nextLine();
+        int minMileage = readInt(scanner, "Please Enter a Min Mileage: ");
+        int maxMileage = readInt(scanner, "Please Enter a Max Mileage: ");
 
         List<Vehicle> vehicles = dealership.getVehiclesByMileage(minMileage, maxMileage);
         displayVehicles(vehicles);
     }
 
     public void processGetByVehicleTypeRequest(Scanner scanner) {
-        System.out.print("Please Enter a Vehicle Type: ");
-        String vehicleType = scanner.nextLine();
+        String vehicleType = readString(scanner, "Please Enter a Vehicle Type: ");
 
         List<Vehicle> vehicles = dealership.getVehiclesByType(vehicleType);
         displayVehicles(vehicles);
@@ -120,58 +107,61 @@ public class UserInterface {
     }
 
     public void processAddVehicleRequest(Scanner scanner) {
-        System.out.println("Please Fill Out All Required Fields");
-        System.out.print("VIN: ");
-        int vin = scanner.nextInt();
-        System.out.print("Year: ");
-        int year = scanner.nextInt();
-        scanner.nextLine();
-        System.out.print("Make: ");
-        String make = scanner.nextLine();
-        System.out.print("Model: ");
-        String model = scanner.nextLine();
-        System.out.print("Vehicle Type: ");
-        String vehicleType = scanner.nextLine();
-        System.out.print("Color: ");
-        String color = scanner.nextLine();
-        System.out.print("Mileage: ");
-        int odometer = scanner.nextInt();
-        System.out.print("Price: ");
-        double price = scanner.nextDouble();
-        scanner.nextLine();
-
-        Vehicle vehicle = new Vehicle(vin, year, make, model, vehicleType, color, odometer, price);
+        System.out.println("\n--- Adding Vehicle ---");
+        Vehicle vehicle = getVehicleInfo(scanner);
         dealership.addVehicle(vehicle);
-        DealershipFileManager manager = new DealershipFileManager();
-        manager.saveDealership(dealership);
-        System.out.println("Vehicle added successfully.");
+        saveAndConfirm("Vehicle added successfully.");
     }
 
     public void processRemoveVehicleRequest(Scanner scanner) {
-        System.out.println("Please Fill Out All Required Fields");
-        System.out.print("VIN: ");
-        int vin = scanner.nextInt();
-        System.out.print("Year: ");
-        int year = scanner.nextInt();
-        scanner.nextLine();
-        System.out.print("Make: ");
-        String make = scanner.nextLine();
-        System.out.print("Model: ");
-        String model = scanner.nextLine();
-        System.out.print("Vehicle Type: ");
-        String vehicleType = scanner.nextLine();
-        System.out.print("Color: ");
-        String color = scanner.nextLine();
-        System.out.print("Mileage: ");
-        int odometer = scanner.nextInt();
-        System.out.print("Price: ");
-        double price = scanner.nextDouble();
-        scanner.nextLine();
+        System.out.println("\n--- Removing Vehicle ---");
+        Vehicle vehicle = getVehicleInfo(scanner);
+        dealership.removeVehicle(vehicle);
+        saveAndConfirm("Vehicle removed successfully.");
+    }
 
-        Vehicle vehicle = new Vehicle(vin, year, make, model, vehicleType, color, odometer, price);
-        dealership.removeVehicle(vehicle);DealershipFileManager manager = new DealershipFileManager();
+    public Vehicle getVehicleInfo(Scanner scanner) {
+        int vin = readInt(scanner, "VIN: ");
+        int year = readInt(scanner, "Year: ");
+        String make = readString(scanner, "Make: ");
+        String model = readString(scanner, "Model: ");
+        String vehicleType = readString(scanner, "Vehicle Type: ");
+        String color = readString(scanner, "Color: ");
+        int odometer = readInt(scanner, "Mileage: ");
+        double price = readDouble(scanner, "Price: ");
+
+        return new Vehicle(vin, year, make, model, vehicleType, color, odometer, price);
+    }
+
+    private void saveAndConfirm(String message) {
+        DealershipFileManager manager = new DealershipFileManager();
         manager.saveDealership(dealership);
         System.out.println("Vehicle removed successfully.");
+    }
+
+    private String readString(Scanner scanner, String prompt) {
+        System.out.print(prompt);
+        return scanner.nextLine().trim();
+    }
+
+    private int readInt(Scanner scanner, String prompt) {
+        while (true) {
+            try {
+                return Integer.parseInt(readString(scanner, prompt));
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input, please enter a whole number.");
+            }
+        }
+    }
+
+    private double readDouble(Scanner scanner, String prompt) {
+        while (true) {
+            try {
+                return Double.parseDouble(readString(scanner, prompt));
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input, please enter a number");
+            }
+        }
     }
 
     private void displayVehicles(List<Vehicle> vehicles) {
